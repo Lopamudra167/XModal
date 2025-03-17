8daf9d6d5cd68a3d084319154f24b675864fd8ba
Lopamudra167 <lmjena1star@gmail.com>
2025-03-17 23:55:12 +0530

    fix some issue

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -1,217 +1,125 @@
-import { useState, useRef, useEffect } from "react";
-import "./modal.css";
+import './Modal.css';
 
-function Modal() {
-    const [open, setOpen] = useState(false);
-    const [form, setForm] = useState({ username: "", email: "", phone: "", dob: "" });
-    const modalRef = useRef(null);
+import React, {
+  Fragment,
+  useRef,
+  useState,
+} from 'react';
 
-    // Close modal when clicking outside
-    useEffect(() => {
-        const handleClickOutside = (e) => {
-            if (modalRef.current && !modalRef.current.contains(e.target)) {
-                setOpen(false);
-            }
-        };
+const Modal = () => {
+  const [open, setOpen] = useState(false);
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    dob: "",
+  });
+  const insideModal = useRef(null);
 
-        if (open) {
-            document.addEventListener("mousedown", handleClickOutside);
-        }
+  // Close modal if clicked outside
+  const handleClose = (e) => {
+    if (insideModal.current && !insideModal.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
 
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [open]);
+  // Validation function
+  const handleValidation = () => {
+    if (form.phone.length !== 10) {
+      alert("Invalid phone number. Please enter a 10-digit phone number.");
+      return false;
+    }
 
-    // Validate phone and date of birth
-    const isValidForm = () => {
-        if (form.phone.length !== 10) {
-            alert("Invalid phone number. Please enter a 10-digit phone number.");
-            return false;
-        }
+    const currentDate = new Date(form.dob);
 
-        const dob = new Date(form.dob);
-        if (isNaN(dob)) {
-            alert("Invalid date of birth. Please enter a valid date.");
-            return false;
-        } 
-        else if (dob > new Date()) {
-            alert("Invalid date of birth. Date of birth cannot be in the future.");
-            return false;
-        }
+    //If currentDate return other than number will comes in NaN
+    if(isNaN(currentDate)){
+        alert("Invalid date of birth. Please enter a valid date.")
+        return false;
+    }
+    else if (currentDate > new Date()) {
+      alert("Invalid date of birth. Date of birth cannot be in the future.");
+      return false;
+    }
+    return true;
+  };
 
-        return true;
-    };
+  //HandleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!handleValidation()) {
+      return;
+    }
 
-    // Handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!isValidForm()) return;  // Stop if validation fails
+    setForm({
+      username: "",
+      email: "",
+      phone: "",
+      dob: "",
+    });
 
-        setForm({ username: "", email: "", phone: "", dob: "" });
-        setOpen(false);
-    };
+    setOpen(false);
+  };
 
-    // Handle input changes
-    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  // Handle all form input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
 
-    return (
-        <>
-            <div className="container">
-                <h1>User Details Modal</h1>
-                <button onClick={() => setOpen(true)}>Open Form</button>
-            </div>
-            {open && (
-                <div className="modal-overlay" onClick={() => setOpen(false)}>
-                    <div className="modal-content" ref={modalRef} onClick={(e) => e.stopPropagation()}>
-                        <h3>Fill Details</h3>
-                        <form onSubmit={handleSubmit}>
-                            <label>Username: </label>
-                            <input type="text" name="username" onChange={handleChange} value={form.username} required />
-                            <br />
-                            <label>Email Address: </label>
-                            <input type="email" name="email" onChange={handleChange} value={form.email} required />
-                            <br />
-                            <label>Phone Number: </label>
-                            <input type="tel" name="phone" onChange={handleChange} value={form.phone} required />
-                            <br />
-                            <label>Date of Birth: </label>
-                            <input type="date" name="dob" onChange={handleChange} value={form.dob} required />
-                            <br />
-                            <button type="submit" className="submit-button">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            )}
-        </>
-    );
-}
+  return (
+    <Fragment>
+      <h1>User Details Modal</h1>
+      <button onClick={() => setOpen(true)}>Open Form</button>
+
+      {open && (
+        <div className="modal" onClick={handleClose}>
+          <div className="modal-content" ref={insideModal}>
+            <h4>Fill Details</h4>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">UserName</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={form.username}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="email">Email Address:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="phone">Phone Number:</label>
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                value={form.phone}
+                onChange={handleChange}
+              />
+              <label htmlFor="dob">Date of Birth:</label>
+              <input
+                type="date"
+                name="dob"
+                id="dob"
+                value={form.dob}
+                onChange={handleChange}
+              />
+              <button className="submit-button" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
-
-
-// import './Modal.css';
-
-// import React, {
-//   Fragment,
-//   useRef,
-//   useState,
-// } from 'react';
-
-// const Modal = () => {
-//   const [open, setOpen] = useState(false);
-//   const [form, setForm] = useState({
-//     username: "",
-//     email: "",
-//     phone: "",
-//     dob: "",
-//   });
-//   const insideModal = useRef(null);
-
-//   // Close modal if clicked outside
-//   const handleClose = (e) => {
-//     if (insideModal.current && !insideModal.current.contains(e.target)) {
-//       setOpen(false);
-//     }
-//   };
-
-//   // Validation function
-//   const handleValidation = () => {
-//     if (form.phone.length !== 10) {
-//       alert("Invalid phone number. Please enter a 10-digit phone number.");
-//       return false;
-//     }
-
-//     const currentDate = new Date(form.dob);
-
-//     //If currentDate return other than number will comes in NaN
-//     if(isNaN(currentDate)){
-//         alert("Invalid date of birth. Please enter a valid date.")
-//         return false;
-//     }
-//     else if (currentDate > new Date()) {
-//       alert("Invalid date of birth. Date of birth cannot be in the future.");
-//       return false;
-//     }
-//     return true;
-//   };
-
-//   //HandleSubmit
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (!handleValidation()) {
-//       return;
-//     }
-
-//     setForm({
-//       username: "",
-//       email: "",
-//       phone: "",
-//       dob: "",
-//     });
-
-//     setOpen(false);
-//   };
-
-//   // Handle all form input changes
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setForm((prev) => ({ ...prev, [name]: value }));
-//   };
-
-//   return (
-//     <Fragment>
-//       <h1>User Details Modal</h1>
-//       <button onClick={() => setOpen(true)}>Open Form</button>
-
-//       {open && (
-//         <div className="modal" onClick={handleClose}>
-//           <div className="modal-content" ref={insideModal}>
-//             <h4>Fill Details</h4>
-//             <form onSubmit={handleSubmit}>
-//               <label htmlFor="username">UserName</label>
-//               <input
-//                 type="text"
-//                 id="username"
-//                 name="username"
-//                 value={form.username}
-//                 onChange={handleChange}
-//                 required
-//               />
-//               <label htmlFor="email">Email Address:</label>
-//               <input
-//                 type="email"
-//                 name="email"
-//                 id="email"
-//                 value={form.email}
-//                 onChange={handleChange}
-//                 required
-//               />
-//               <label htmlFor="phone">Phone Number:</label>
-//               <input
-//                 type="number"
-//                 name="phone"
-//                 id="phone"
-//                 value={form.phone}
-//                 onChange={handleChange}
-//               />
-//               <label htmlFor="dob">Date of Birth:</label>
-//               <input
-//                 type="date"
-//                 name="dob"
-//                 id="dob"
-//                 value={form.dob}
-//                 onChange={handleChange}
-//               />
-//               <button className="submit-button" type="submit">
-//                 Submit
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       )}
-//     </Fragment>
-//   );
-// };
-
-// export default Modal;
